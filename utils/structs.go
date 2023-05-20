d7e958640f82c86661762dd58a6c20af1bc8ebe7
Ishwarya <ishwarya@jaam.co>
2023-05-21 00:26:51 +0530

    Odds bysport struct modified

--- utils/structs.go
+++ utils/structs.go
@@ -268,9 +268,37 @@ type PickStatistics struct {
 }
 
 type OddsBySport struct {
-	DataId string `json:"dataId"`
-	Data   string `json:"data"`
+	DataId string  `json:"dataId"`
+	Data   []Event `json:"data"`
 }
+type Outcome struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type Market struct {
+	Key        string    `json:"key"`
+	LastUpdate string    `json:"last_update"`
+	Outcomes   []Outcome `json:"outcomes"`
+}
+
+type Bookmaker struct {
+	Key        string   `json:"key"`
+	Title      string   `json:"title"`
+	LastUpdate string   `json:"last_update"`
+	Markets    []Market `json:"markets"`
+}
+
+type Event struct {
+	ID           string      `json:"id"`
+	SportKey     string      `json:"sport_key"`
+	SportTitle   string      `json:"sport_title"`
+	CommenceTime string      `json:"commence_time"`
+	HomeTeam     string      `json:"home_team"`
+	AwayTeam     string      `json:"away_team"`
+	Bookmakers   []Bookmaker `json:"bookmakers"`
+}
+
 type NotificationsStack struct {
 	NotificationId      string    `json:"notificationId"`
 	NotificationType    string    `json:"notificationType"`
